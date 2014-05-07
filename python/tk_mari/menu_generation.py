078cf1e4970a5fb850c763ddc75dae0b56cccb26
Tim Chauncey <tim.chauncey@windmilllanevfx.com>
2014-05-07 11:55:36 +0100

    Added creation of context menu

--- python/tk_mari/menu_generation.py
+++ python/tk_mari/menu_generation.py
@@ -1,4 +1,7 @@
+import os
+import sys
 import mari
+from tank.platform.qt import QtGui, QtCore
 
 class MenuGenerator(object):
     def __init__(self, engine):
@@ -7,16 +10,80 @@ class MenuGenerator(object):
     def create_menu(self):
         shotgun_menu = 'MainWindow/Shotgun'
 
+        context_menu = self._add_context_menu(shotgun_menu)
+
         # now enumerate all items and create menu objects for them
         menu_items = []
         for (cmd_name, cmd_details) in self._engine.commands.items():
-            menu_items.append( AppCommand(cmd_name, cmd_details) )
+            menu_items.append(AppCommand(cmd_name, cmd_details))
 
         for cmd in menu_items:
-            cmd.add_command_to_menu(shotgun_menu)
+            if cmd.get_type() == 'context_menu':
+                cmd.add_command_to_menu(context_menu)
+            else:
+                cmd.add_command_to_menu(shotgun_menu)
+
 
     def destroy_menu(self):
         mari._menu_commands = {}
+
+        shotgun_actions = mari.menus.actions('MainWindow', 'Shotgun')
+        for action in shotgun_actions:
+            mari.menus.removeAction('MainWindow/Shotgun/%s' % action.name())
+
+    def _add_context_menu(self, menu):
+        mari._menu_commands = {}
+        ctx = self._engine.context
+        ctx_name = str(ctx)
+
+        command_name = 'Jump To File System'
+        mari._menu_commands[command_name] = self._jump_to_fs
+        callback_string = 'mari._menu_commands["%s"]()' % command_name
+
+        action = mari.actions.create(command_name, callback_string)
+        mari.menus.addAction(action, '%s/%s' % (menu, ctx_name))
+
+        command_name = 'Jump To Shotgun'
+        mari._menu_commands[command_name] = self._jump_to_sg
+        callback_string = 'mari._menu_commands["%s"]()' % command_name
+
+        action = mari.actions.create(command_name, callback_string)
+        mari.menus.addAction(action, '%s/%s' % (menu, ctx_name))
+
+        return "%s/%s" % (menu, ctx_name)
+
+    def _jump_to_sg(self):
+        """
+        Jump to shotgun, launch web browser
+        """
+        url = self._engine.context.shotgun_url
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl(url))
+
+    def _jump_to_fs(self):
+
+        """
+        Jump from context to FS
+        """
+        # launch one window for each location on disk
+        paths = self._engine.context.filesystem_locations
+        for disk_location in paths:
+
+            # get the setting
+            system = sys.platform
+
+            # run the app
+            if system == "linux2":
+                cmd = 'xdg-open "%s"' % disk_location
+            elif system == "darwin":
+                cmd = 'open "%s"' % disk_location
+            elif system == "win32":
+                cmd = 'cmd.exe /C start "Folder" "%s"' % disk_location
+            else:
+                raise Exception("Platform '%s' is not supported." % system)
+
+            exit_code = os.system(cmd)
+            if exit_code != 0:
+                self._engine.log_error("Failed to launch '%s'!" % cmd)
 
 
 class AppCommand(object):
