13837e504191c2eb38cc8e606882b962bafe42ca
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2020-08-26 13:46:04 -0400

    black!

--- python/tk_mari/menu_generation.py
+++ python/tk_mari/menu_generation.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2014 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
@@ -19,35 +19,37 @@ import mari
 import sgtk
 from sgtk.platform.qt import QtGui, QtCore
 
+
 class ActionFactory(object):
     """
     Factory class that handles creation of Mari actions that wrap Toolkit command
     callbacks.
-    
+
     Mari's actions take a string to execute rather than a callback.  This means we
     have to store the callbacks somewhere that Mari can find to execute them from
     by name.
-    
+
     Therefore, we store them in a special attribute patched onto the mari module
     which is then cleaned up when the menu gets destroyed.
     """
+
     def __init__(self):
         """
         Construction
         """
         self.__action_id = 0
-    
+
     # The special Toolkit attribute that we will use to store our callbacks so that
     # Mari can find and execute them
     ACTION_COMMANDS_ATTR = "_shotgun_menu_callbacks"
-    
+
     def create_action(self, name, callback):
         """
         Create a Mari action for the specified callback
-        
+
         :param name:        The name of the action/Toolkit command
         :param callback:    The callback that should be run when the action is executed
-        
+
         :returns:           A Mari Action that will execute the Toolkit callback
         """
         # we store the callbacks on the Mari module so that the Mari Actions can
@@ -66,11 +68,14 @@ class ActionFactory(object):
         all_commands = getattr(mari, ActionFactory.ACTION_COMMANDS_ATTR)
         all_commands[action_id] = lambda: QtCore.QTimer.singleShot(100, callback)
         setattr(mari, ActionFactory.ACTION_COMMANDS_ATTR, all_commands)
-        callback_string = "mari.%s[%d]()" % (ActionFactory.ACTION_COMMANDS_ATTR, action_id)
-        
+        callback_string = "mari.%s[%d]()" % (
+            ActionFactory.ACTION_COMMANDS_ATTR,
+            action_id,
+        )
+
         # return a new action that will execute this callback:
         return mari.actions.create(name, callback_string)
-    
+
     def clear(self):
         """
         Clear the list of Toolkit actions stored on the mari module
@@ -78,15 +83,17 @@ class ActionFactory(object):
         if hasattr(mari, ActionFactory.ACTION_COMMANDS_ATTR):
             delattr(mari, ActionFactory.ACTION_COMMANDS_ATTR)
 
+
 class MenuGenerator(object):
     """
     Class used by the Mari engine to build the Shotgun menu from the various
     commands registered by the apps that have been loaded.
     """
+
     def __init__(self, engine):
         """
         Construction
-        
+
         :param engine:    The toolkit engine that owns this menu generator
         """
         self._engine = engine
@@ -100,8 +107,11 @@ class MenuGenerator(object):
         Create the Shotgun menu
         """
         self._engine.log_debug("Creating the Shotgun menu for Mari...")
-        
-        shotgun_menu = "%s/%s" % (MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT)
+
+        shotgun_menu = "%s/%s" % (
+            MenuGenerator.MAIN_MENU_SET,
+            MenuGenerator.SHOTGUN_MENU_ROOT,
+        )
 
         context_menu = self.__build_context_menu(shotgun_menu)
         mari.menus.addSeparator(shotgun_menu)
@@ -109,7 +119,7 @@ class MenuGenerator(object):
         # now enumerate all items and create menu objects for them
         menu_items = []
         for (cmd_name, cmd_details) in self._engine.commands.items():
-            #if not (cmd_name == "Shotgun File Manager..." or cmd_name == "Version up Current Scene..."):
+            # if not (cmd_name == "Shotgun File Manager..." or cmd_name == "Version up Current Scene..."):
             menu_items.append(AppCommand(cmd_name, cmd_details, self.__action_factory))
 
         commands_by_app = {}
@@ -126,38 +136,56 @@ class MenuGenerator(object):
 
         self.__build_app_menu(commands_by_app, shotgun_menu)
 
-
     def destroy_menu(self):
         """
-        Clean up the Shotgun menu by removing all items.  
-        
-        Note that currently this doesn't delete the root 'Shotgun' menu or any 
-        sub-menus that have been created under it (it does remove the items in 
+        Clean up the Shotgun menu by removing all items.
+
+        Note that currently this doesn't delete the root 'Shotgun' menu or any
+        sub-menus that have been created under it (it does remove the items in
         the sub-menus though).  This is because there doesn't seem to be an API
         that does this!
-        
+
         This has been logged with the Foundry (#2014080310000042).
-    
+
         """
         self._engine.log_debug("Destroying the Shotgun menu for Mari...")
-        
+
         # clear the action factory:
         self.__action_factory.clear()
 
         # Remove all menu actions from sub-menus - note that this doesn't currently
         # remove the actual sub-menus - how do we do that?
-        sub_menus = mari.menus.submenus(MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT)
+        sub_menus = mari.menus.submenus(
+            MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT
+        )
         for sm in sub_menus:
-            actions = mari.menus.actions(MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT, sm)
+            actions = mari.menus.actions(
+                MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT, sm
+            )
             for action in actions:
-                mari.menus.removeAction("%s/%s/%s/%s" % (MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT, 
-                                                         sm, action.name()))
-        
+                mari.menus.removeAction(
+                    "%s/%s/%s/%s"
+                    % (
+                        MenuGenerator.MAIN_MENU_SET,
+                        MenuGenerator.SHOTGUN_MENU_ROOT,
+                        sm,
+                        action.name(),
+                    )
+                )
+
         # remove all actions from the Shotgun root menu:
-        actions = mari.menus.actions(MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT)
+        actions = mari.menus.actions(
+            MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT
+        )
         for action in actions:
-            mari.menus.removeAction("%s/%s/%s" % (MenuGenerator.MAIN_MENU_SET, MenuGenerator.SHOTGUN_MENU_ROOT, 
-                                                  action.name()))        
+            mari.menus.removeAction(
+                "%s/%s/%s"
+                % (
+                    MenuGenerator.MAIN_MENU_SET,
+                    MenuGenerator.SHOTGUN_MENU_ROOT,
+                    action.name(),
+                )
+            )
 
     def _jump_to_sg(self):
         """
@@ -179,11 +207,11 @@ class MenuGenerator(object):
 
             # run the app
             if system == "linux2":
-                cmd = "xdg-open \"%s\"" % disk_location
+                cmd = 'xdg-open "%s"' % disk_location
             elif system == "darwin":
-                cmd = "open \"%s\"" % disk_location
+                cmd = 'open "%s"' % disk_location
             elif system == "win32":
-                cmd = "cmd.exe /C start \"Folder\" \"%s\"" % disk_location
+                cmd = 'cmd.exe /C start "Folder" "%s"' % disk_location
             else:
                 raise Exception("Platform '%s' is not supported." % system)
 
@@ -194,9 +222,9 @@ class MenuGenerator(object):
     def __build_app_menu(self, commands_by_app, shotgun_menu):
         """
         Build the main app menu for all app commands
-        
+
         :param commands_by_app:    A dictionary of commands to add to the main Shotgun menu
-        :param shotgun_menu:       The full path to the shotgun menu         
+        :param shotgun_menu:       The full path to the shotgun menu
         """
         for app_name in sorted(commands_by_app.keys()):
             if len(commands_by_app[app_name]) > 1:
@@ -209,18 +237,17 @@ class MenuGenerator(object):
                 cmd = commands_by_app[app_name][0]
                 cmd.add_to_menu(shotgun_menu)
 
-
     def __build_context_menu(self, shotgun_menu):
         """
         Build the standard context menu with jump-to-file-system & jump-to-shotgun
         commands
-        
+
         :param shotgun_menu:    The full path to the shotgun menu
         :returns:               Returns the path to the context menu
         """
         ctx = self._engine.context
         ctx_name = str(ctx)
-        
+
         # Because there is currently no API for removing empty sub-menus (see foundry
         # issue #2014080310000042) it's not a good idea to create the normal work
         # are sub-menu as switching context will result in the menu being confusing
@@ -235,22 +262,28 @@ class MenuGenerator(object):
         # When this is possible, the context menu should be created with the context
         # name in-line with the rest of the engines:
         # ctx_menu = "%s/%s" % (shotgun_menu, ctx_name)
-        
-        action = self.__action_factory.create_action("Jump To File System", self._jump_to_fs)
+
+        action = self.__action_factory.create_action(
+            "Jump To File System", self._jump_to_fs
+        )
         mari.menus.addAction(action, ctx_menu)
-        action = self.__action_factory.create_action("Jump To Shotgun", self._jump_to_sg)
+        action = self.__action_factory.create_action(
+            "Jump To Shotgun", self._jump_to_sg
+        )
         mari.menus.addAction(action, ctx_menu)
 
         return ctx_menu
+
 
 class AppCommand(object):
     """
     Wrapper for a command registered by an app
     """
+
     def __init__(self, name, command_dict, action_factory):
         """
         Construction
-        
+
         :param name:            The name of the command
         :param command_dict:    A dictionary of parameters specified for the command
         :param action_factory:  An instance of the ActionFactory class used to create
@@ -266,17 +299,17 @@ class AppCommand(object):
     def get_app_name(self):
         """
         Find the display name for the app this command was loaded from
-        
+
         :returns:    The app display name if known
         """
         if "app" not in self.properties:
-            return None 
+            return None
         return self.properties["app"].display_name
 
     def get_app_instance_name(self):
         """
         Find the instance name for the app this command was loaded from
-        
+
         :returns:    The app instance name if known
         """
         if "app" not in self.properties:
@@ -295,7 +328,7 @@ class AppCommand(object):
     def get_type(self):
         """
         Find the type of this command
-        
+
         :returns:    The type of the command or 'default' if not found
         """
         return self.properties.get("type", "default")
@@ -303,17 +336,15 @@ class AppCommand(object):
     def add_to_menu(self, menu):
         """
         Add this command to the menu
-        
+
         :param menu:    The menu this command should be added to.
         """
         # create a new action for the command if needed:
         if not self.__action:
-            self.__action = self.__action_factory.create_action(self.name, self.callback)
-        
+            self.__action = self.__action_factory.create_action(
+                self.name, self.callback
+            )
+
         if self.__action:
             # add the action to the menu:
             mari.menus.addAction(self.__action, menu)
-
-
-
-
