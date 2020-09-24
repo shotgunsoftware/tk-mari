ee88ea81ed15a4f881a368370a2e76ef360b14bc
Jean-François Boismenu <jfboismenu@users.noreply.github.com>
2020-09-24 11:10:54 -0400

    Update init.py

--- startup/init.py
+++ startup/init.py
@@ -13,11 +13,6 @@ from __future__ import print_function
 import os
 import mari
 
-# HACK: Do not merge. It appears the Python 3 version of Mari does not properly added PYTHONPATH to sys.path
-import sys
-
-sys.path.insert(0, os.environ["PYTHONPATH"])
-
 
 def show_warning(msg):
     """
