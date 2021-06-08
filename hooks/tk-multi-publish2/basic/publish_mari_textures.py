f01d347a875044eff4a936121399e54c38ec5c1d
Sergio Zamudio <sergio.zamudio@autodesk.com>
2021-06-08 09:25:59 -0500

    sg-22648-shotgrid replace shotgun instances

--- hooks/tk-multi-publish2/basic/publish_mari_textures.py
+++ hooks/tk-multi-publish2/basic/publish_mari_textures.py
@@ -48,8 +48,8 @@ class MariTexturesPublishPlugin(HookBaseClass):
         loader_url = "https://support.shotgunsoftware.com/hc/en-us/articles/219033078"
 
         return """
-        Publishes the file to Shotgun. A <b>Publish</b> entry will be
-        created in Shotgun which will include a reference to the file's current
+        Publishes the file to ShotGrid. A <b>Publish</b> entry will be
+        created in ShotGrid which will include a reference to the file's current
         path on disk. Other users will be able to access the published file via
         the <b><a href='%s'>Loader</a></b> so long as they have access to
         the file's location on disk.
@@ -62,7 +62,7 @@ class MariTexturesPublishPlugin(HookBaseClass):
         file to the next version after publishing.
 
         The <code>version</code> field of the resulting <b>Publish</b> in
-        Shotgun will also reflect the version number identified in the filename.
+        ShotGrid will also reflect the version number identified in the filename.
         The basic worklfow recognizes the following version formats by default:
 
         <ul>
