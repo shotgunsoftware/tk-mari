751c0127c86f66a164022e0b3dbd8f066e973434
Julien Langlois <16244608+julien-lang@users.noreply.github.com>
2024-05-02 14:03:12 -0700

    SG-35128 Now support version 7.x (#26)
    * Now support version 7.x
    
    * Update badge

--- engine.py
+++ engine.py
@@ -70,7 +70,7 @@ class MariEngine(sgtk.platform.Engine):
 
         # check that this version of Mari is supported:
         MIN_VERSION = (4, 0, 0)  # completely unsupported below this!
-        MAX_VERSION = (6, 0)  # untested above this so display a warning
+        MAX_VERSION = (7, 0)  # untested above this so display a warning
 
         mari_version = mari.app.version()
         if mari_version.major() < MIN_VERSION[0] or (
